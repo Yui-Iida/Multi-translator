6c61e420a50c44aba2396e7e0c3c6862963f474b
Yui Iida <yui@Yui.fritz.box>
2023-08-10 13:44:12 +0200

    fix styles and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "./src/*.{html, js}"],
+  content: ["index.html", "./src/script.js"],
   darkMode: "class",
   theme: {
     extend: {
